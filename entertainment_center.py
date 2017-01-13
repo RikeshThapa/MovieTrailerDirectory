2cafd691a808f132a4d05efa86167f1acbba0dd4
Rikesh Thapa <rthapa@u.rochester.edu>
2017-01-13 00:37:01 -0800

    adding new instance

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,4 +5,9 @@ toy_story = media.Movie("Toy Story",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
+avatar = media.Movie("Avatar",
+                     "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+                     "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_.jpg",
+                     "http://www.imdb.com/title/tt0499549/videoplayer/vi531039513?ref_=tt_ov_vi")
+
 print(toy_story.storyline)
